275648176be7c29d10281504b5de839f7a1af379
Justout <jkajustin@gmail.com>
2025-05-16 22:16:01 -0500

    options menu can only be moved by keys/controller inputs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
@@ -15,8 +16,10 @@ public class InputManager : MonoBehaviour
     public bool strafeLeft { get; private set; }
     public bool interact { get; private set; }
     public bool decline { get; private set; }
-    private InputAction moveForwardAction, moveBackwardsAction, turnRightAction, turnLeftAction, strafeRightAction, strafeLeftAction, interactAction, declineAction;
-
+    public bool options { get; private set; }
+    public bool menu { get; private set; }
+    private InputAction moveForwardAction, moveBackwardsAction, turnRightAction, turnLeftAction, strafeRightAction, strafeLeftAction, interactAction, declineAction, optionsAction, menuAction;
+    public EventSystem eventSystem;
     private void Awake()
     {
         if (instance == null)
@@ -40,6 +43,8 @@ public class InputManager : MonoBehaviour
         strafeLeft = strafeLeftAction.WasPressedThisFrame();
         interact = interactAction.WasPressedThisFrame();
         decline = declineAction.WasPressedThisFrame();
+        options = optionsAction.WasPressedThisFrame();
+        menu = menuAction.WasPerformedThisFrame();
     }
 
 
@@ -53,6 +58,8 @@ public class InputManager : MonoBehaviour
         strafeLeftAction = playerInput.actions["StrafeLeft"];
         interactAction = playerInput.actions["Interact"];
         declineAction = playerInput.actions["Decline"];
+        optionsAction = playerInput.actions["Options"];
+        menuAction = playerInput.actions["Menu"];
     }
 
 }
