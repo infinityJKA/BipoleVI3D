8e7ab0a15132f6a3eb9484b88dafd140fc083771
Justout <jkajustin@gmail.com>
2025-05-16 19:13:55 -0500

    Improved input system plus controller support

---
+++ Assets/Scripts/DungeonInputCheck.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerController))]
+public class DungeonInputCheck : MonoBehaviour
+{
+
+    private PlayerController controller;
+
+    // public KeyCode forward = KeyCode.W,
+    // backward = KeyCode.S,
+    // strafeLeft = KeyCode.Q,
+    // strafeRight = KeyCode.E,
+    // turnLeft = KeyCode.A,
+    // turnRight = KeyCode.D,
+    // interact = KeyCode.Space,
+    // retract = KeyCode.LeftShift;
+
+    private void Awake()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (controller.inputState == DungeonInputControlState.FreeMove)
+        {
+            // if (Input.GetKeyDown(forward)) controller.Walk(0, 1);
+            // if (Input.GetKeyDown(backward)) controller.Walk(0, -1);
+            // if (Input.GetKeyDown(strafeLeft)) controller.Walk(-1, 0);
+            // if (Input.GetKeyDown(strafeRight)) controller.Walk(1, 0);
+            // if (Input.GetKeyDown(turnLeft)) controller.RotateLeft();
+            // if (Input.GetKeyDown(turnRight)) controller.RotateRight();
+            // if (Input.GetKeyDown(interact)) controller.Interact();
+
+            if (InputManager.instance.moveForward) controller.Walk(0, 1);
+            else if (InputManager.instance.moveBackward) controller.Walk(0, -1);
+            else if (InputManager.instance.strafeRight) controller.Walk(1, 0);
+            else if (InputManager.instance.strafeLeft) controller.Walk(-1, 0);
+            else if (InputManager.instance.turnRight) controller.RotateRight();
+            else if (InputManager.instance.turnLeft) controller.RotateLeft();
+            else if (InputManager.instance.interact) controller.Interact();
+
+        }
+        else if (controller.inputState == DungeonInputControlState.Dialogue)
+        {
+            // if (Input.GetKeyDown(interact)) controller.ProgressDialogue();
+
+            if (InputManager.instance.interact) controller.ProgressDialogue();
+
+        }
+
+    }
+}
+
+public enum DungeonInputControlState
+{
+    FreeMove,Dialogue,Combat,Menu
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+    [SerializeField] PlayerInput playerInput;
+    public bool moveForward { get; private set; }
+    public bool moveBackward { get; private set; }
+    public bool turnRight { get; private set; }
+    public bool turnLeft { get; private set; }
+    public bool strafeRight { get; private set; }
+    public bool strafeLeft { get; private set; }
+    public bool interact { get; private set; }
+    public bool decline { get; private set; }
+    private InputAction moveForwardAction, moveBackwardsAction, turnRightAction, turnLeftAction, strafeRightAction, strafeLeftAction, interactAction, declineAction;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        SetInputActions();
+    }
+
+    private void Update()
+    {
+        moveForward = moveForwardAction.WasPressedThisFrame();
+        moveBackward = moveBackwardsAction.WasPressedThisFrame();
+        turnRight = turnRightAction.WasPressedThisFrame();
+        turnLeft = turnLeftAction.WasPressedThisFrame();
+        strafeRight = strafeRightAction.WasPressedThisFrame();
+        strafeLeft = strafeLeftAction.WasPressedThisFrame();
+        interact = interactAction.WasPressedThisFrame();
+        decline = declineAction.WasPressedThisFrame();
+    }
+
+
+    private void SetInputActions() // connects variables on awake
+    {
+        moveForwardAction = playerInput.actions["WalkForwardsDungeon"];
+        moveBackwardsAction = playerInput.actions["WalkBackwardsDungeon"];
+        turnRightAction = playerInput.actions["TurnRightDungeon"];
+        turnLeftAction = playerInput.actions["TurnLeftDungeon"];
+        strafeRightAction = playerInput.actions["StrafeRight"];
+        strafeLeftAction = playerInput.actions["StrafeLeft"];
+        interactAction = playerInput.actions["Interact"];
+        declineAction = playerInput.actions["Decline"];
+    }
+
+}
